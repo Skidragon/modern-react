33a306aa37a628715dbefd19802eb3b144f9b297
Simon Davis <Simon_Freelance@hotmail.com>
2019-02-01 18:49:19 -0500

    Finished exercise 2

--- src/exercises/02.js
+++ src/exercises/02.js
@@ -2,14 +2,18 @@
 import React, {useState} from 'react'
 
 // 🐨 create a function here called useCounter
+function useCounter() {
+  const [count, setCount] = useState(0)
+  const incrementCount = () => setCount(count + 1)
+
+  return {count, incrementCount}
+}
 // the "use" prefix is a convention, and not required.
 // don't overthink this. It's JavaScript :)
 // 💰 make sure to export it for the tests.
 
 function Counter() {
-  // 🐨 move these two lines to your function and return what you need
-  const [count, setCount] = useState(0)
-  const incrementCount = () => setCount(count + 1)
+  const {count, incrementCount} = useCounter()
   return <button onClick={incrementCount}>{count}</button>
 }
 
